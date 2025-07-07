1072ab64e38a917fd72d96563a8d8cc4c54ed91a
isavmei <isabela.avelar@gmail.com>
2025-07-07 13:03:24 -0300

    código em C salvo para teste :rat:

---
+++ bee1430.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int soma_dos_valores(int notas[p]; int soma_valor)
+ for (int contador = 0, contador < notas[p], contador++){
+     while notas != '/'{
+         for (int contagem
+     }
+ }
+int main() {
+    int W = 1;
+    int H = 1/2;
+    int Q = 1/4;
+    int E = 1/8;
+    int S = 1/16;
+    int T = 1/32;
+    int X = 1/64;
+    // a soma deve ser um 
+    int notas = [200];
+    
+    scanf("%c", &notas[p]);
+    int contador = 0;
+    //contar as notas
+    //somar as notas
+    //verificar se da um 
+    //se der um, somar.
+    
+    for (int contador = 0; contador < notas[p], contador++){
+        while (notas[p] != '/'){
+        soma = 0;
+        for (int contador2 = 0; contador2 < contador; contador2++) {
+            soma += vetor[contador2];
+        } if (soma == 1) {
+            valor_total 
+        }
+        }
+    }
+
+    return 0;
+}
